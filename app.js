95c977c8a44966fa06eff6d8a506de0fe555d650
richardonguitar <richardonguitar@gmail.com>
2024-03-19 22:11:44 +0900

    GUI finished but sound doesn't play when key is pushed

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let pianoContainer = document.getElementsByClassName('piano-container');
-const base = './audio';
+const base = './audio/';
 window.onload = () => {
   //24keys
-  for(let index=1;index <=24; index++) {
+  for(let index = 1;index <= 24; index++) {
     let div = document.createElement('div');
     div.classList.add('key', index<10? 'black-key': 'white-key');
+
+    const number = index <=9 ? "0" + index : index;
+    div.addEventListener('click', () => {
+      new Audio(`${base}key${number}.mp3`).play();
+    })
     pianoContainer[0].appendChild(div);
   }
 }
